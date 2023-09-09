822490fb874a879363e98294a118007e8e075020
Abdul Hakim <hakim@undercurrent.tech>
2023-09-09 10:57:18 +0700

    added price feed component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 import { AppProps } from "next/app";
 import { WagmiConfig, configureChains, createConfig, sepolia } from "wagmi";
